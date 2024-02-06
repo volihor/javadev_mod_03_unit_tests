b58c7f13560fdda60f56c4ffd27799d3e32b627f
volihor <you@example.com>
2024-02-06 18:05:15 +0200

    Added 3 tests for sum(n), where n equal/return: 1/1; 3/6; 0/"IllegalArgumentException".

--- src/main/java/global/goit/SumCalculator.java
+++ src/main/java/global/goit/SumCalculator.java
@@ -1,38 +1,24 @@
 package global.goit;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class SumCalculator {
 
-    public static void main(String[] args) {
-        int numUser;
-        SumCalculator sumCalculator = new SumCalculator();
-        numUser = sumCalculator.scanUserInput();
-        System.out.println(sumCalculator.sum(numUser));
+    public int sum(int n) {
+        return arithmeticProgression(n);
     }
 
-    private int scanUserInput() {
-        int userInput = -1;
-        Scanner scan = new Scanner(System.in);
-        do {
-            System.out.println(" *** Please enter some number from 1 to maxInt :) inclusive.");
-            try {
-                userInput = scan.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println(" *** You entered Invalid data.");
-                // To avoid the loop on the next iteration ! Because scan.nextByte() reads the same token again and throws the same exception again.
-                scan.next();
-            }
-            if (userInput == 0) {
-                throw new IllegalArgumentException();
-            }
-        } while (userInput < 0 || userInput >= Integer.MAX_VALUE);
-        return userInput;
+    private int arithmeticProgression(int n) {
+        if (checkValidInput(n)) {
+            return (n + n * n) / 2;
+        } else
+            return -1;  // Return -1 as an example of invalid input.
     }
 
-    private int sum(int n) {
-
-        return (n + n * n) / 2;
+    private static boolean checkValidInput(int n) {
+        if (n == 0) {
+            throw new IllegalArgumentException();
+        } else if (n < 1 || n > Integer.MAX_VALUE) {
+            return false;
+        }
+        return true;
     }
 }
--- src/test/java/global/goit/SumCalculatorTest.java
+++ src/test/java/global/goit/SumCalculatorTest.java
@@ -1,8 +1,35 @@
 package global.goit;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class SumCalculatorTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+    private static SumCalculator sumCalc;
+
+    @BeforeEach
+    void init() {
+        sumCalc = new SumCalculator();
+    }
+
+    @Test
+    void sumOneTest() {
+        assertEquals(1, sumCalc.sum(1));
+    }
+
+    @Test
+    void sumThreeTest() {
+        assertEquals(6,sumCalc.sum(3));
+    }
+
+    @Test
+    void sumZeroExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            sumCalc.sum(0);
+        });
+//        assertEquals(0,sumCalc.sum(0));
     }
 }
